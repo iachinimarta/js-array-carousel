f4056c74291fe533a620ea509d14571b47403bab
Marta Iachini <iachinimarta@gmail.com>
2022-07-20 14:35:39 +0200

    Modifiche: - refactoring - aggiunti commenti

--- js/main.js
+++ js/main.js
@@ -8,43 +8,58 @@ const imgContainerDom = document.querySelector('.img-container');
 for (let i = 0; i < imageList.length; i++) {
 
     // Dentro img-container vado a inserire il div contenente le img da stampare e al posto di src metto il nome che ho dato all array così è dinamico
-    imgContainerDom.innerHTML += `<div class="images"> <img class="img" src="${imageList[i]}"> </div>`;
-
+    imgContainerDom.innerHTML +=    `<div class="images"><img class="image" src="${imageList[i]}"></div>`;
 }
 
-// Imposto una variabile in cui dichiaro che l'img che sarà visibile sarà in posizione 0
-let imgShow = 0;
+// Imposto una variabile in cui dichiaro che l'img che sarà visibile(active) sarà in posizione 0
+let active = 0;
 
 // Richiamo tutti gli elementi in img-container che hanno la classe "images"
-let imgContainerElements = document.getElementsByClassName('images');
+let imagesList = document.getElementsByClassName('images');
 
 // Aggiungo all'elemento in posizione 0 contenuto in img-container la classe "show" che lo renderà visibile
-imgContainerElements[imgShow].classList.add('show');
+imagesList[active].classList.add('show');
 
-// Richiamo il pulsante 'up' dall'html
 const btnUp = document.querySelector('.up');
+const btnDown = document.querySelector('.down');
 
 // Aggiungo un event listener al pulsante 'up' in modo tale da poterci associare un'azione
 btnUp.addEventListener('click',
     function () {
         // Vado all'elemento in posizione 0 e rimuovo la classe "show"
-        imgContainerElements[imgShow].classList.remove('show');
+        imagesList[active].classList.remove('show');
 
-        // Incremento il valore (la posizione) di imgShow (quindi dell'img che dovrà essere visibile)
-        imgShow++;
+        // Incremento il valore (la posizione) di active (quindi dell'img che dovrà essere visibile)
+        active++;
 
         // Aggiungo nuovamente la classe "show" all'elemento incrementato di 1
-        imgContainerElements[imgShow].classList.add('show');
+        imagesList[active].classList.add('show');
+
+        //Nel momento in cui avanziamo con le slide il pulsante per andare indietro deve ricomparire
+        btnDown.classList.remove('hidden');
+
+        //Se arriviamo all ultima slide il pulsante per andare avanti deve scomparire
+        if (active == imagesList.length - 1) {
+            btnUp.classList.add('hidden');
+        }
     }
 );
 
 // Faccio lo stesso procedimento per il pulsante 'down'
-const btnDown = document.querySelector('.down');
+
 
 btnDown.addEventListener('click',
     function () {
-        imgContainerElements[imgShow].classList.remove('show');
-        imgShow--;
-        imgContainerElements[imgShow].classList.add('show');
+        imagesList[active].classList.remove('show');
+        active--;
+        imagesList[active].classList.add('show');
+
+        //Quando torno indietro dall ultima slide il pulsante UP deve ricomparire
+        btnUp.classList.remove('hidden');
+
+        //Quando siamo tornati alla prima slide il pulsante DOWN deve scomparire di nuovo
+        if (active == 0) {
+            btnDown.classList.add('hidden');
+        }
     }
 );
